c427c99d1d26393b76245a3b31b32a88fbed8c16
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-09-03 02:53:42 +0600

    design done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 $(function () {
   'use strict';
 
+  //navbar fixed
+  window.addEventListener("scroll", function () {
+    var scrolling = $(this).scrollTop();
+    if (scrolling > 200) {
+      $("#header").addClass("sticky");
+    } else {
+      $("#header").removeClass("sticky");
+    }
+  });
+
+  // bottom To top
+  window.addEventListener("scroll", function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 700) {
+      $(".bottomToTop").show();
+    } else {
+      $(".bottomToTop").hide();
+    }
+  });
+
 
   // WoW Js
-  var wow = new WOW(
-    {
-      offset: 2,
-      mobile: false
-    }
-  )
+  var wow = new WOW({
+    offset: 2,
+    mobile: false
+  })
   wow.init();
 
 });
